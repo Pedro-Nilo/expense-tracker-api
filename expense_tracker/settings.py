e1103e1dbd48c56da59f7ae47c3c0e3c2595dfd9
Pedro-Nilo <pedroniloz.nicola@gmail.com>
2021-08-15 19:53:42 -0300

    Installed django-rest-framework.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -94,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-BR"
 
 TIME_ZONE = "UTC"
 
